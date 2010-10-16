6ef7edb47d4607a9cec6e795cc2b8722f3350a8e
Gavin Walters <gavinwalters@shaw.ca>
2010-10-16 00:19:03 -0600

    Added a pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 RealEstateEmpire::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
