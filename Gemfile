24d87ab76fd243a0bc907225804ecb6d544ffb30
Gavin Walters <gavinwalters@shaw.ca>
2010-10-15 23:50:51 -0600

    Initial Commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.1'
+gem 'sqlite3-ruby', :require => 'sqlite3'
+
+
+group :development do
+   gem 'rspec-rails', '2.0.0'
+end
+
+group :test do
+  gem 'rspec', '2.0.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RealEstateEmpire::Application.config.secret_token = 'b62234f833b360df4358430f3c84a2d82b117a406d4120f1d69266aab7a0661ad448bc9272e7b8517f9c75bb22b6c4cf0ef0f45a5657c2b4184afd925ef7cd5d'
