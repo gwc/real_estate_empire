e35366bc4e887681d05ec82ad70dba641b2eb187
Gavin Walters <gavinwalters@shaw.ca>
2011-01-08 23:06:27 -0700

    Working user signin

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,8 @@ class SessionsController < ApplicationController
         @title = "Sign in"
         render 'new'
     else
-      #Handle successful signin
+     sign_in user
+     redirect_to user
     end
     
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 # == Schema Information
-# Schema version: 20101111041102
+# Schema version: 20101111044528
 #
 # Table name: users
 #
@@ -9,6 +9,7 @@
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -33,15 +34,22 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
   
-  def User.authenticate(email, submitted_password)
-    user = find_by_email(email)
-    return nil    if user.nil?
-    return user   if user.has_password?(submitted_password)
+  class << self
+    def User.authenticate(email, submitted_password)
+      user = find_by_email(email)
+      (user && user.has_password?(submitted_password)) ? user : nil
+    end
+  
+    def authenticate_with_salt(id, cookie_salt)
+      user = find_by_id(id)
+      (user && user.salt == cookie_salt) ? user : nil
+    end
   end
   
   
   
   private
+  
   def encrypt_password
     self.salt = make_salt if new_record?
     self.encrypted_password = encrypt(password)
